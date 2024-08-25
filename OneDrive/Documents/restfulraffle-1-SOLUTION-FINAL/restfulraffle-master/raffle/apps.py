3d9cde4a32517ce28d431630b2205008e6ef3ebf
Patricia Rumbidzai Manhenda <99598105+Rumbie-Manhenda@users.noreply.github.com>
2024-08-25 20:45:22 +0300

    Delete OneDrive directory

--- OneDrive/Documents/restfulraffle-1-SOLUTION-FINAL/restfulraffle-master/raffle/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class RaffleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'raffle'
-
-def ready(self):
-    import raffle.signals  
